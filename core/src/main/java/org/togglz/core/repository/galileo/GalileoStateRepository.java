971d3cd0c85bf4b97c2e1e4926f91eb5fca64968
Ahsan Amin <ahsan.amin@Ahsans-Macbook.local>
2023-10-03 18:27:47 +0500

    ENG-18965 added the galileo repository to togglz

--- core/src/main/java/org/togglz/core/repository/galileo/GalileoStateRepository.java
+++ core/src/main/java/org/togglz/core/repository/galileo/GalileoStateRepository.java
@@ -1,5 +1,10 @@
 package org.togglz.core.repository.galileo;
 
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.util.Map;
+
 import org.togglz.core.Feature;
 import org.togglz.core.logging.Log;
 import org.togglz.core.logging.LogFactory;
@@ -7,51 +12,78 @@ import org.togglz.core.repository.FeatureState;
 import org.togglz.core.repository.StateRepository;
 
 import com.careem.galileo.sdk.Galileo;
-//import org.togglz.core.repository.jdbc.JDBCStateRepository;
 
 import java.util.HashMap;
+import org.togglz.core.util.Strings;
 
 public class GalileoStateRepository implements StateRepository {
 
     protected final Galileo galileo;
 
-    protected final String serviceName;
-
     protected final Log log = LogFactory.getLog(GalileoStateRepository.class);
 
-    public GalileoStateRepository(String serviceName) {
-        this.serviceName = serviceName.toLowerCase().replace("-", "_");
-        System.out.println("Name of service before:");
-        System.out.println(serviceName);
-        System.out.println("Name of service after:");
-        System.out.println(this.serviceName);
-
-
-        this.galileo = Galileo.builder()
-                .withService(this.serviceName)
-                .build();
-
-        System.out.println("-----------------created galielo state repo object----------\n\n\n\n");
+    public GalileoStateRepository(Galileo galileoObj) {
+        this.galileo = galileoObj;
     }
 
     @Override
     public FeatureState getFeatureState(Feature feature) {
 
         String inputVariable = "togglz/" + feature.name().toLowerCase();
-        String value = this.galileo.getString(inputVariable, new HashMap<>(), "");
 
-        System.out.println("\n\n---------------------get state called---------------\n\n");
-        System.out.println(value);
-        System.out.println("\n\n");
+        HashMap<String, Object> context = new HashMap<String, Object>(){{put("name", inputVariable);}};
+        String value = this.galileo.getString(inputVariable, context, "failed to fetch");
+
+        if (value == "failed to fetch") {
+            log.warn("Could not fetch the value of " + inputVariable);
+            return null;
+        }
+
+        try {
+            JSONParser parser = new JSONParser();
+            Map<String, Object> jsonMap = (Map<String, Object>) parser.parse(value);
 
 
-        return new FeatureState(new Feature() {
-            @Override
-            public String name() {
-                return null;
+            if(jsonMap.containsKey("FEATURE_ENABLED")) {
+                boolean enabled = (long) jsonMap.get("FEATURE_ENABLED") > 0;
+
+                FeatureState state = new FeatureState(feature, enabled);
+
+                if(jsonMap.containsKey("STRATEGY_ID")) {
+                    String strategyId = (String) jsonMap.get("STRATEGY_ID");
+                    if (Strings.isNotBlank(strategyId)) {
+                        state.setStrategyId(strategyId.trim());
+                    }
+                }
+
+                if(jsonMap.containsKey("STRATEGY_PARAMS")) {
+                    String paramData = (String) jsonMap.get("STRATEGY_PARAMS");
+                    if (Strings.isNotBlank(paramData)) {
+                        try {
+                            Map<String, Object> strategy_params = (Map<String, Object>) parser.parse(paramData);
+
+                            for (Map.Entry<String, Object> entry : strategy_params.entrySet()) {
+                                state.setParameter(entry.getKey(), (String) entry.getValue());
+                            }
+                        }
+                        catch(ParseException e) {
+                            log.warn("Cannot parse strategy params for variable: " + inputVariable);
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+                return state;
             }
-        });
-//        return null;
+
+        }
+        catch (ParseException e) {
+            log.warn("Cannot parse strategy params for variable: " + inputVariable);
+            e.printStackTrace();
+        }
+
+        log.warn("Cannot parse the return value of variable: " + inputVariable);
+        return null;
     }
 
     @Override
